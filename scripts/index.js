8a5c7b0bf0851e5c5fac737e20ef7fda9b799b8a
Ivan Suraev <sega1012vaan@yandex.ru>
2024-04-23 00:10:44 +0500

    1.0

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,23 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template');
 
-// @todo: DOM узлы
+const cardList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
-
+function createCard(initialCard){
+  const card = cardTemplate.content.cloneNode(true);
+  card.querySelector('.card__image').setAttribute('src', initialCard.link);;
+  card.querySelector('.card__title').textContent = initialCard.name;
+  card.querySelector('.card__delete-button').addEventListener('click', removeCard);
+  
+  return card
+}
 // @todo: Функция удаления карточки
-
+function removeCard(evt){
+  const eventTarget = evt.target;
+  const cardItem = eventTarget.closest('card');
+  cardItem.remove();
+}
 // @todo: Вывести карточки на страницу
+initialCards.forEach(element => {
+  cardList.append(createCard(element, removeCard));
+})
