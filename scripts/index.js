c01d1459aef05b70a371104cd07895b07ee4b83b
Ivan Suraev <sega1012vaan@yandex.ru>
2024-04-23 18:27:42 +0500

    1.2

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,23 @@
 const cardTemplate = document.querySelector('#card-template');
 
-const cardList = document.querySelector('.places__list');
+const cardsContainer = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
-function createCard(initialCard){
+function createCard(cardData, onDelete){
   const card = cardTemplate.content.cloneNode(true);
-  card.querySelector('.card__image').setAttribute('src', initialCard.link);;
-  card.querySelector('.card__title').textContent = initialCard.name;
-  card.querySelector('.card__delete-button').addEventListener('click', removeCard);
+  card.querySelector('.card__image').src = cardData.link;
+  card.querySelector('.card__image').alt = `На фото изображено место ${cardData.name}`;
+  card.querySelector('.card__title').textContent = cardData.name;
+  card.querySelector('.card__delete-button').addEventListener('click', onDelete);
   return card
 }
 // @todo: Функция удаления карточки
-function removeCard(evt){
-  const eventTarget = evt.target;
-  const card = eventTarget.closest('.card');
-  card.remove();
-}
+function onDelete(evt){ 
+  const eventTarget = evt.target; 
+  const card = eventTarget.closest('.card'); 
+  card.remove(); 
+} 
 // @todo: Вывести карточки на страницу
 initialCards.forEach(element => {
-  cardList.append(createCard(element, removeCard));
+  cardsContainer.append(createCard(element, onDelete));
 })
